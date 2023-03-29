7b73dc81da9aa53504166ab83151df120a28f2c4
nikhilt <nikhil.t1226@gmail.com>
2023-03-29 15:13:56 +0100

    \ |"?

--- aws-parameters/main.tf
+++ aws-parameters/main.tf
@@ -1,4 +1,4 @@
-resource "aws_ssm_parameter" "parameters" {
+resource "aws_ssm_parameter" "parameter" {
   count  = length(var.parameters)
   name   = var.parameters[count.index].name
   type   = var.parameters[count.index].type
