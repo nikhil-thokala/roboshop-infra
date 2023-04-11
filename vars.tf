60598806a69f9d44a51add5c98f20e37ad864ab1
nikhilt <nikhil.t1226@gmail.com>
2023-04-11 21:49:29 +0100

    \ |"?

--- vars.tf
+++ vars.tf
@@ -1,8 +1,8 @@
 variable "env" {}
 variable "tags" {}
-variable "vpc" {}
 variable "default_vpc_id" {}
 variable "default_route_table" {}
+variable "vpc" {}
 variable "docdb" {}
 variable "rds" {}
 variable "elasticache" {}
