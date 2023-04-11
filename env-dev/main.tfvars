d56787e9e03d065ebe0dc2c03a730f738ed6c1b3
nikhilt <nikhil.t1226@gmail.com>
2023-04-11 16:05:38 +0100

    \ |"?

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -66,3 +66,12 @@ docdb = {
 
   }
 }
+
+rds ={
+  main = {
+    engine                  = "aurora-mysql"
+    engine_version          = "5.7.mysql_aurora.2.11.1"
+    backup_retention_period = 1
+    preferred_backup_window = "07:00-09:00"
+  }
+}
--- main.tf
+++ main.tf
@@ -28,6 +28,15 @@ module "docdb" {
   instance_class           = each.value["instance_class"]
 }
 
-output "vpc" {
-  value = local.db_subnet_ids
-}
+module "rds" {
+  source   = "git::https://github.com/nikhil-thokala/tf-module-rds.git"
+  env      = var.env
+  tags     = var.tags
+
+  subnet_ids               = local.db_subnet_ids
+
+  for_each                 = var.rds
+  engine                   = each.value["engine"]
+  engine_version           = each.value["engine_version"]
+  backup_retention_period  = each.value["backup_retention_period"]
+  preferred_backup_window  = each.value["preferred_backup_window"]
