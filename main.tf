ff10c50be5b943fe1fae23e59b25cdd47d8310d4
nikhilt <nikhil.t1226@gmail.com>
2023-04-12 16:24:00 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -24,6 +24,7 @@ module "docdb" {
   backup_retention_period  = each.value["backup_retention_period"]
   preferred_backup_window  = each.value["preferred_backup_window"]
   skip_final_snapshot      = each.value["skip_final_snapshot"]
+  final_snapshot_identifier = each.value["final_snapshot_identifier"]
   no_of_instances          = each.value["no_of_instances"]
   instance_class           = each.value["instance_class"]
 }
