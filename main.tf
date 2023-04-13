15ef2a0deb92b93e4fe984eb411c176718f9bb55
nikhilt <nikhil.t1226@gmail.com>
2023-04-13 15:18:11 +0100

    \ |"?

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 module "vpc" {
-  source   = "git::https://github.com/nikhil-thokala/tf-module-vpc.git"
-  env      = var.env
-  tags     = var.tags
-  default_route_table = var.default_route_table
-  default_vpc_id = var.default_vpc_id
+  source                    = "git::https://github.com/nikhil-thokala/tf-module-vpc.git"
+  env                       = var.env
+  tags                      = var.tags
+  default_route_table       = var.default_route_table
+  default_vpc_id            = var.default_vpc_id
 
   for_each        = var.vpc
   vpc_cidr        = each.value["vpc_cidr"]
