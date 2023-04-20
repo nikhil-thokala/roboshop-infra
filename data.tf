559acd40d5ea5cbf9a6e84a2b8cd697d30b7c8c6
nikhilt <nikhil.t1226@gmail.com>
2023-04-20 23:57:22 +0100

    rds

--- data.tf
+++ data.tf
@@ -1,6 +1,6 @@
-data "aws_ssm_parameter" "ssh_pass" {
-  name = "${var.env}.shh.pass"
-}
+#data "aws_ssm_parameter" "ssh_pass" {
+#  name = "${var.env}.shh.pass"
+#}
 
 data "aws_ami" "ami" {
   most_recent = true
